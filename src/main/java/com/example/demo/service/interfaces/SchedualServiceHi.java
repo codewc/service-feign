a1046e7fd8993fa1ad40909605d53c2c6b32c5d8
wangchun <wangchun@DESKTOP-F0028ND>
2018-05-01 17:20:07 +0800

    建立项目

---
+++ src/main/java/com/example/demo/service/interfaces/SchedualServiceHi.java
@@ -0,0 +1,20 @@
+package com.example.demo.service.interfaces;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 测试类
+ * 
+ * @author wangchun
+ *
+ */
+@FeignClient(value="eureka-client")
+public interface SchedualServiceHi {
+
+	@RequestMapping(value = "/hi", method = { RequestMethod.GET})
+	String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+}
